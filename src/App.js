d70d6d6a352aad182224d37eba04b14d7f17c644
Daniyar Bashenov <danyuwa@MBP-Daniyar.lan>
2024-05-11 03:14:07 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        
+      </div>
+      <h1>Тойға шақыру</h1>
+      <div className="photo">
+
+      </div>
+      <div className="form">
+        
+        <h3>
+          
+        </h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
