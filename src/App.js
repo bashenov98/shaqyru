5809ada36936ab9497cdb20a8d40172ff5ccde84
Daniyar Bashenov <danyuwa@MacBook-Pro-Daniyar.local>
2024-05-13 16:57:50 +0600

    integrated google sheets

--- src/App.js
+++ src/App.js
@@ -12,6 +12,12 @@ import axios from 'axios';
 function App() {
   const [selectedValue, setSelectedValue] = useState({});
   const [inputText, setInputText] = useState('');
+  const [mobile, setMobile] = useState('');
+  const [formData, setFormData] = useState({
+    name: '',
+    mobile: '',
+    answer: ''
+  });
 
   const handleChange = (event) => {
     setSelectedValue(event.target.value);
@@ -21,15 +27,30 @@ function App() {
     setInputText(event.target.value);
   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const handleChangeMobile = (event) => {
+    setMobile(event.target.value);
+  };
+
+  const Submit = async (e) => {
+    e.preventDefault();
+    const formEle = document.querySelector("form");
+    const formDatab = new FormData(formEle);
 
     const token = '7101981316:AAGrugzrt7e25mIkbzl_ee8ZO8INAFsrst0';
     const text = (`${inputText}: ${selectedValue}`);
 
+    setFormData({ Name: inputText, Mobile: '87719052133', Answer: selectedValue })
+
     const response = await axios.get(`https://api.telegram.org/bot${token}/sendMessage?chat_id=-4241267745&text=${text}`, {
     });
+    const response2 = await axios.post('https://script.google.com/macros/s/AKfycbxQd66HA8lMwLTC-unavVzbzhynHSf7hBL0IUJ_jvAXaViC3kw_yVhyzTPi1RdvX0Uqrg/exec',  formDatab, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    }
+    )
     console.log(response)
+    console.log(response2)
   };
 
   return (
@@ -52,14 +73,14 @@ function App() {
       </div>
       <div>
         <h2>Мекенжайымыз:</h2>
-        <h4>Алматы қаласы,<br/>
-          Ремизовка, Арайлы көшесі, 16<br/>
-          “Eleven HOTEL&HALL”,<br/>
+        <h4>Алматы қаласы,<br />
+          Ремизовка, Арайлы көшесі, 16<br />
+          “Eleven HOTEL&HALL”,<br />
           BALLROOM</h4>
       </div>
 
       <div className="form">
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={(e) => Submit(e)}>
           <h3>Тойға келетініңізді растауыңызды сұраймыз</h3>
           <h4>Жұбайыңызбен келетін болсаңыз, екі есімді де көрсетіңіз</h4>
           <TextField
@@ -68,10 +89,21 @@ function App() {
             value={inputText}
             onChange={handleChangeText}
             margin="normal"
+            name='Name'
             fullWidth
           />
 
-          <RadioGroup aria-labelledby="radio-group" name="radio-buttons" value={selectedValue} onChange={handleChange}>
+          <TextField
+            label="Телефон нөміріңіз"
+            variant="outlined"
+            value={mobile}
+            onChange={handleChangeMobile}
+            margin="normal"
+            name='Mobile'
+            fullWidth
+          />
+
+          <RadioGroup aria-labelledby="radio-group" name="Answer" value={selectedValue} onChange={handleChange}>
             <FormControlLabel value="Әрине, келемін" control={<Radio />} label="Әрине, келемін" />
             <FormControlLabel value="Әлі белгісіз" control={<Radio />} label="Әлі белгісіз" />
             <FormControlLabel value="Өкінішке орай, келе алмаймын" control={<Radio />} label="Өкінішке орай, келе алмаймын" />
