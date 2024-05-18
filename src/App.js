fbc7fdb4cdf4a65fd099a1c4d64493f6b2cd78cc
e-Madi.Abzhanov <e-Madi.Abzhanov@homecredit.kz>
2024-05-19 03:38:26 +0500

    Modified page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 
 
 import './App.css';
-import RadioGroup from '@mui/material/RadioGroup';
-import Button from '@mui/material/Button';
-import FormControlLabel from '@mui/material/FormControlLabel'
+import './index.css';
+import './fonts.css';
+
 import { useState } from 'react';
-import Radio from '@mui/material/Radio'
-import TextField from '@mui/material/TextField'
 import axios from 'axios';
 
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Radio from '@mui/material/Radio';
+
+import OyuLeft from 'assets/oyu-left.svg';
+import OyuRight from 'assets/oyu-right.svg';
+import OyuMid from 'assets/oyu-mid.svg'
+import MapImg from 'assets/map.svg';
+import OyuTopImg from 'assets/oyu-top-half.svg';
+import OyuBtmImg from 'assets/oyu-bottom-half.svg';
+import CoupleImg from 'assets/couple-img.jfif';
+
 function App() {
+
+  const radioBtnProp = {
+    color: "white",
+    '&.Mui-checked': {
+      color: "white",
+    }
+  };
+
   const [selectedValue, setSelectedValue] = useState({});
   const [inputText, setInputText] = useState('');
   const [mobile, setMobile] = useState('');
@@ -43,7 +61,7 @@ function App() {
 
     const response = await axios.get(`https://api.telegram.org/bot${token}/sendMessage?chat_id=-4241267745&text=${text}`, {
     });
-    const response2 = await axios.post('https://script.google.com/macros/s/AKfycbxQd66HA8lMwLTC-unavVzbzhynHSf7hBL0IUJ_jvAXaViC3kw_yVhyzTPi1RdvX0Uqrg/exec',  formDatab, {
+    const response2 = await axios.post('https://script.google.com/macros/s/AKfycbxQd66HA8lMwLTC-unavVzbzhynHSf7hBL0IUJ_jvAXaViC3kw_yVhyzTPi1RdvX0Uqrg/exec', formDatab, {
       headers: {
         'Content-Type': 'multipart/form-data'
       }
@@ -55,62 +73,70 @@ function App() {
 
   return (
     <div className="App">
-      <div className="header">
 
-      </div>
-      <h1>Тойға шақыру</h1>
-      <div className="photo">
-      </div>
+      <img src={OyuLeft} className='oyuLeft' alt='oyu-ornek' />
+      <img src={OyuRight} className='oyuRight' alt='oyu-ornek' />
+      <div className='topPage'>
+        <h2 style={{ marginTop: "110px" }}>Тойға шақыру</h2>
+        <h3 className='mainSubtitle'>DANIYAR & MAKPAL</h3>
 
-      <div>
+        <div className='heartBorder'>
+          <img src={CoupleImg} className='heartImg' alt='Daniyar and Makpal img' />
+        </div>
+
+
+        <h2>Құрметті қонақтар</h2>
+        <h3>Сіздерді тойымыздың қадірлі қонағы болуға шақырамыз! </h3>
+
+        <img src={OyuMid} className='oyuMid' alt='oyu-ornek' />
+
         <h2>Той иелері:</h2>
         <h3>Жамантай-Гүлсұлу</h3>
-      </div>
-      <div>
-        <h2>Той салтанаты</h2>
-        <h4>Күні: 10.08.2024</h4>
-        <h4>Басталуы: 17:00</h4>
-      </div>
-      <div>
+
+        <h2 style={{ marginTop: "40px" }}>Той салтанаты:</h2>
+        <h3>Күні: 10/08/2024 </h3>
+        <h3 style={{ marginBottom: "40px" }}>Басталуы: 17:00</h3>
+
         <h2>Мекенжайымыз:</h2>
-        <h4>Алматы қаласы,<br />
-          Ремизовка, Арайлы көшесі, 16<br />
-          “Eleven HOTEL&HALL”,<br />
-          BALLROOM</h4>
+        <h3>Алматы қаласы,</h3>
+        <h3>Ремизовка, Арайлы көшесі, 16</h3>
+        <h3 >“Eleven Hotel&Hall”,</h3>
+        <h3 style={{ marginBottom: "40px" }}>Ballroom</h3>
+
+        <img src={MapImg} className='mapImg' alt='oyu-ornek' />
+        <img src={OyuTopImg} className='oyuTopImg' alt='oyu-ornek' />
+
       </div>
-
-      <div className="form">
+      <div className='btmPage'>
+        <img src={OyuBtmImg} className='oyuBtmImg' alt='oyu-ornek' />
         <form onSubmit={(e) => Submit(e)}>
-          <h3>Тойға келетініңізді растауыңызды сұраймыз</h3>
-          <h4>Жұбайыңызбен келетін болсаңыз, екі есімді де көрсетіңіз</h4>
-          <TextField
-            label="Аты-жөніңіз"
-            variant="outlined"
-            value={inputText}
-            onChange={handleChangeText}
-            margin="normal"
-            name='Name'
-            fullWidth
-          />
+          <h2 className='formHeader'>Тойға келетініңізді растауыңызды сұраймыз</h2>
+          <h3 className='formSubHeader'>Жұбайыңызбен келетін болсаңыз, екі есімді де көрсетіңіз</h3>
+          <div className='inputField'>
+            <input
+              placeholder="Аты-жөніңіз"
+              value={inputText}
+              onChange={handleChangeText}
+              name='name'
+            />
+          </div>
 
-          <TextField
-            label="Телефон нөміріңіз"
-            variant="outlined"
-            value={mobile}
-            onChange={handleChangeMobile}
-            margin="normal"
-            name='Mobile'
-            fullWidth
-          />
+          <div className='inputField'>
+            <input
+              placeholder="Телефон нөміріңіз"
+              value={mobile}
+              onChange={handleChangeMobile}
+              name='mobile'
+            />
+          </div>
 
           <RadioGroup aria-labelledby="radio-group" name="Answer" value={selectedValue} onChange={handleChange}>
-            <FormControlLabel value="Әрине, келемін" control={<Radio />} label="Әрине, келемін" />
-            <FormControlLabel value="Әлі белгісіз" control={<Radio />} label="Әлі белгісіз" />
-            <FormControlLabel value="Өкінішке орай, келе алмаймын" control={<Radio />} label="Өкінішке орай, келе алмаймын" />
+            <FormControlLabel value="Әрине, келемін" control={<Radio sx={radioBtnProp} />} label="Әрине, келемін" />
+            <FormControlLabel value="Әлі белгісіз" control={<Radio sx={radioBtnProp} />} label="Әлі белгісіз" />
+            <FormControlLabel value="Өкінішке орай, келе алмаймын" control={<Radio sx={radioBtnProp} />} label="Өкінішке орай, келе алмаймын" />
           </RadioGroup>
-          <div className='submitButton'>
-            <Button type="submit" variant="contained" color='success'>Жіберу</Button>
-          </div>
+
+          <button type="submit" variant="contained">Жіберу</button>
         </form>
       </div>
     </div>
