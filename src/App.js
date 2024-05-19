0dff106547ad66a1e132b5b10f9226587f66b915
Daniyar Bashenov <danyuwa@MacBook-Pro-Daniyar.local>
2024-05-19 22:24:14 +0600

    link header and photo updated

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import OyuMid from 'assets/oyu-mid.svg'
 import MapImg from 'assets/map.svg';
 import OyuTopImg from 'assets/oyu-top-half.svg';
 import OyuBtmImg from 'assets/oyu-bottom-half.svg';
-import CoupleImg from 'assets/IMG_5898.JPG';
+import CoupleImg from 'assets/IMG_5793.JPG';
 
 function App() {
 
